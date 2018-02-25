e966ab2889b8199b42202def63d9363a7f4e31fa
Ulises <ulimartinez96@gmail.com>
2018-02-24 18:22:44 -0700

    added controller for camera

--- camserver/cam_controller.py
+++ camserver/cam_controller.py
@@ -3,6 +3,14 @@ import time
 import picamera
 from PIL import Image
 import zbar
+# import the necessary packages
+import argparse
+import datetime
+import imutils
+import time
+import cv2
+from picamera import PiCamera
+from picamera.array import PiRGBArray
 
 
 def readQR(stream):
@@ -22,4 +30,89 @@ def readQR(stream):
     for symbol in image:
         # do something useful with results
         return symbol.data
+
+
+def saveData(frame, x, x1, y, y1):
+    qr_text = readQR(frame[y:y1, x:x1])
+    print(qr_text)
+
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-v", "--video", help="path to the video file")
+ap.add_argument("-a", "--min-area", type=int, default=500, help="minimum area size")
+args = vars(ap.parse_args())
+
+# if the video argument is None, then we are reading from webcam
+if args.get("video", None) is None:
+    camera = PiCamera()
+    camera.resolution = (640, 480)
+    camera.framerate = 30
+    rawCapture = PiRGBArray(camera, size=(640, 480))
+    time.sleep(0.25)
+
+# otherwise, we are reading from a video file
+else:
+    camera = cv2.VideoCapture(args["video"])
+    # initialize the first frame in the video stream
+
+firstFrame = None
+# loop over the frames of the video
+for image in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
+    # grab the current frame and initialize the occupied/unoccupied
+    # text
+    # frame = camera.read()
+    frame = image.array
+
+    rawCapture.truncate(0)
+
+    # resize the frame, convert it to grayscale, and blur it
+    frame = imutils.resize(frame, width=500)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+    # if the first frame is None, initialize it
+    if firstFrame is None:
+        firstFrame = gray
+        continue
+    # compute the absolute difference between the current frame and
+    # first frame
+    frameDelta = cv2.absdiff(firstFrame, gray)
+    thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+
+    # dilate the thresholded image to fill in holes, then find contours
+    # on thresholded image
+    thresh = cv2.dilate(thresh, None, iterations=2)
+    (cnts, _) = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+                                 cv2.CHAIN_APPROX_SIMPLE)
+
+    changeFrame = None
+    # loop over the contours
+    for c in cnts:
+        # if the contour is too small, ignore it
+        if cv2.contourArea(c) < args["min_area"]:
+            continue
+
+        # compute the bounding box for the contour, draw it on the frame,
+        # and update the text
+        (x, y, w, h) = cv2.boundingRect(c)
+        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        saveData(frame, x, x + w, y, y + h)
+
+
+    # show the frame and record if the user presses a key
+    cv2.imshow("Security Feed", frame)
+    cv2.imshow("Thresh", thresh)
+    cv2.imshow("Frame Delta", frameDelta)
+    if changeFrame is not None:
+        cv2.imshow("change", changeFrame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break
+
+# cleanup the camera and close any open windows
+# camera.release()
+cv2.destroyAllWindows()
+
 
